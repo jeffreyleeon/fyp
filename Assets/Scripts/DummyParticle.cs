43c53e2d638b3ccb248299aa49b2aa873d1658ac
Lee Chun On <jeffreyleeon@gmail.com>
2016-08-04 11:16:33 +0800

    Add particle for testing hands

---
+++ Assets/Scripts/DummyParticle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyParticle : MonoBehaviour {
+
+	Vector3 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			print("Up key is held down");
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			transform.position = initialPos;
+		}
+	}
+}
