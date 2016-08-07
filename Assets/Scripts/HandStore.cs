72fc3d490e4e6dd0f36d3bdc603c51007444ae07
johnny <cywongaw@stu.ust.hk>
2016-08-07 15:39:05 +0800

    hand store & hand listener skeleton

---
+++ Assets/Scripts/HandStore.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class HandStore{
+
+	private static HandStore instance = new HandStore();
+	private HandModel[] allhands;
+	private int handnum;
+
+	private HandStore(){
+		allhands = null;
+		handnum = 0;
+	}
+
+	public static HandStore getInstance(){
+		return instance;
+	}
+
+	// set hands
+	public void setHands(HandModel[] hands){
+		if (hands.Length != 0 || handnum != 0) {
+			allhands = new HandModel[hands.Length];
+			hands.CopyTo (allhands, 0);
+			handnum = hands.Length;
+			Debug.Log (handnum);
+		}
+	}
+
+
+
+	// check if hand is open
+	public bool IsOpenHand(int hand){
+
+
+		return true;
+	}
+
+	// check if hand appear
+	public bool IsHandAppear(){
+		if (handnum == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
