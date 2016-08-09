a995ab272135d16fa832c228818c21d3f1803672
johnny <cywongaw@stu.ust.hk>
2016-08-09 21:25:12 +0800

    add ShootingController (in progress)

---
+++ Assets/Scripts/LeapUnityUtil.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Leap {
+	public class LeapUnityUtil {
+		public static Vector3 toUnityvector3(Vector3 position){
+			return new Vector3 (-position.x, position.z, position.y);
+		}
+	}
+}
