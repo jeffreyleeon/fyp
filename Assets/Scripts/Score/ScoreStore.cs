b78cd8530354c92a690694ccb1997070801fadbf
Lee Chun On <jeffreyleeon@gmail.com>
2016-08-17 20:57:32 +0800

    Add dummy ScoreStore class

---
+++ Assets/Scripts/Score/ScoreStore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class ScoreStore{
+
+	private static ScoreStore instance = new ScoreStore();
+
+	private ScoreStore () {
+	}
+
+	public static ScoreStore GetInstance () {
+		return instance;
+	}
+}
