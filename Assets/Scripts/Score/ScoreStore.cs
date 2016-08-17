0909fe9ff64bde9723604f4d7905d379f4c4cb9c
Lee Chun On <jeffreyleeon@gmail.com>
2016-08-17 21:24:15 +0800

    Implement SetScore and GetScore functions

--- Assets/Scripts/Score/ScoreStore.cs
+++ Assets/Scripts/Score/ScoreStore.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * ScoreStore deal with players' score for local game and online game
+ */
 public sealed class ScoreStore{
 
 	private static ScoreStore instance = new ScoreStore();
+	private string singlePlayerKey = "SINGLE_PLAYER_KEY";
 
 	private ScoreStore () {
 	}
@@ -11,4 +15,15 @@ public sealed class ScoreStore{
 	public static ScoreStore GetInstance () {
 		return instance;
 	}
+
+	public void SetScore (float score) {
+		PlayerPrefs.SetFloat (singlePlayerKey, score);
+	}
+
+	public float GetScore () {
+		return PlayerPrefs.GetFloat (singlePlayerKey, 0.0f);
+	}
+
+	// TODO: Deal with online game, score from server
+	// TODO: Deal with multi-player game (Optional)
 }
