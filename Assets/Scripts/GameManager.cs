3e6c5c518509ff5da8910b8b8aca2c6c2be86c71
johnny <cywongaw@stu.ust.hk>
2016-08-23 15:58:42 +0800

    make enemy spawn network aware

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class GameManager : MonoBehaviour{
 	GameObject trinus;
 	GameObject player;
+	GameObject enemyManager;
 
 	void Awake(){
 		trinus = GameObject.Find ("Trinus");
+		enemyManager = GameObject.Find ("EnemyManager");
+
 	}
 
 	// Use this for initialization
 	void Start () {
 		player = PhotonNetwork.Instantiate ("Player", new Vector3(0, 0, 0), Quaternion.identity, 0);
 		trinus.transform.parent = player.transform;
+		enemyManager.GetComponent<Spawn> ().enabled = true;
 	}
 	
 	// Update is called once per frame
