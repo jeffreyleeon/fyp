ec7da80ee2acebce99dc91364aed40e85411830a
johnny <cywongaw@stu.ust.hk>
2016-08-31 17:48:43 +0800

    Add HittableObject.cs

---
+++ Assets/Scripts/HittableObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class HittableObject : Photon.MonoBehaviour {
+
+	public int maxHealth;
+
+	private int currentHealth;
+
+
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+
+	#region public method
+	/// <summary>
+	/// Gets the current health.
+	/// </summary>
+	/// <returns>The current health.</returns>
+	public  int GetCurrentHealth(){
+		return currentHealth;
+	}
+
+
+	/// <summary>
+	/// Object get hit by damage.
+	/// </summary>
+	/// <returns><c>true</c>, if object still alive, <c>false</c> otherwise.</returns>
+	/// <param name="damage">Damage.</param>
+	public virtual void HitBy(int damage){
+		if (currentHealth < 0) {
+			Debug.LogError ("Object has negative health already");
+		}
+		currentHealth -= damage;
+		if (currentHealth < 0) {
+			currentHealth = 0;
+		}
+	}
+
+	/// <summary>
+	/// Kill this object.
+	/// </summary>
+	public virtual void Kill(){
+		PhotonNetwork.Destroy (this.gameObject);
+	}
+
+	public bool Alive(){
+		return currentHealth == 0;
+	}
+
+	#endregion
+
+}
