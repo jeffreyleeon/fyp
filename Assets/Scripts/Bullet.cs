9ce5e80389f2815f104408b7d66dd84bd09e2633
johnny <cywongaw@stu.ust.hk>
2016-09-01 13:10:04 +0800

    Add Bullet class

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhotonView))]
+public class Bullet : Photon.MonoBehaviour {
+
+	[Tooltip("Damage it can deal")]
+	public int attack = 100;
+
+
+}
