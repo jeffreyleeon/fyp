fb90a919d8f70e6fbc6cf655c51022b57170e678
johnny <cywongaw@stu.ust.hk>
2016-09-01 21:34:35 +0800

    add bullet.cs and attach to bullet prefab

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,5 +7,11 @@ public class Bullet : Photon.MonoBehaviour {
 	[Tooltip("Damage it can deal")]
 	public int attack = 100;
 
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag ("Enemy")) {
+			col.gameObject.GetComponent<HittableObject> ().Kill();
+
+		}
+	}
 
 }
