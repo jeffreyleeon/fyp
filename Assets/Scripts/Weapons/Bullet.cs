2d2dd2dd505bfb6692face358ccf4770130eba30
johnny <cywongaw@stu.ust.hk>
2016-09-11 09:44:31 +0800

    remove photonView and onCollistionEnter() from bullet

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -1,17 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(PhotonView))]
-public class Bullet : Photon.MonoBehaviour {
+public class Bullet : MonoBehaviour {
 
 	[Tooltip("Damage it can deal")]
 	public int attack = 100;
 
-	void OnCollisionEnter(Collision col){
-		if (col.gameObject.CompareTag (ObjectStore.GetEnemyTag())) {
-			col.gameObject.GetComponent<HittableObject> ().Kill();
-
-		}
-	}
 
 }
