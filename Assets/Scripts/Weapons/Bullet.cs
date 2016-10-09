846fb82ab6558644ef7c4ea6781dce53dad193a7
Lee Chun On <jeffreyleeon@gmail.com>
2016-10-09 11:31:46 +0800

    Update bullet attack API

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
 	[Tooltip("Damage it can deal")]
-	public int attack = 100;
+	public int attack = 20;
 
 	//public for debugging, will be private
 	public string owner;
 
 	public void SetOwner(string ownerName){
 		owner = ownerName;
+	}
+
+	public int Attack {
+		get {
+			return attack;
+		}
 	}
 }
