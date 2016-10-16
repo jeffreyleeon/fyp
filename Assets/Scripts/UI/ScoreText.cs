70e87839d8d5b70a9c7c15db27688e87ca81ab36
Alice Sin <sinwinglamalice@gmail.com>
2016-10-17 01:27:23 +0800

    Update ScoreText script

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -4,16 +4,11 @@ using System.Collections;
 
 public class ScoreText : MonoBehaviour {
 
-	private Text txt;
-
-	// Use this for initialization
-	void Start () {
-		txt = gameObject.GetComponent<Text> ();
-		txt.text = "0";
-	}
+	public Text txt;
+	private int score = 0;
 	
-	// Update is called once per frame
 	void Update () {
-		txt.text = "Hello";
+//		score = Scoreboard.GetLocalPlayerScore ();
+		txt.text = score.ToString ();
 	}
 }
