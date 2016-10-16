efe7b6001e3b543e766effb3110d625e6aa6b909
Alice Sin <sinwinglamalice@gmail.com>
2016-10-17 01:16:15 +0800

    Add Score Text script

---
+++ Assets/Scripts/UI/ScoreText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+
+	private Text txt;
+
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text> ();
+		txt.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text = "Hello";
+	}
+}
