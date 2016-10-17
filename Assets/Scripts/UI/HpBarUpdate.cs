42eabca3a217e90ba766d584f26e64351844d00b
Alice Sin <sinwinglamalice@gmail.com>
2016-10-17 23:20:37 +0800

    Add health bar script (dummy)

---
+++ Assets/Scripts/UI/HpBarUpdate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HpBarUpdate : MonoBehaviour {
+
+	public RectTransform hpBar;
+	private int Hp = 100;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hpBar.sizeDelta = new Vector2( hpBar.rect.width - 0.2f, hpBar.rect.height);
+		print ("Hehe================" + hpBar.rect.width);
+	}
+}
