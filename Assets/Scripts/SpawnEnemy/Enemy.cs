44cae7f62a0c0b8ec7d9be40418fe96561c4ed3a
johnny <cywongaw@stu.ust.hk>
2016-10-23 17:39:48 +0800

    add getAllPlayerScore(), need to use namespace FYP.Score when doing score related operation.

--- Assets/Scripts/SpawnEnemy/Enemy.cs
+++ Assets/Scripts/SpawnEnemy/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using FYP.Score;
 
 public class Enemy : HittableObject {
 
--- Assets/Scripts/Utils/Scoreboard.cs
+++ Assets/Scripts/Utils/Scoreboard.cs
@@ -1,14 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public sealed class Scoreboard : MonoBehaviour {
 
-	public static void  AddLocalPlayerScore(int score){
-		PhotonNetwork.player.AddScore (score);
-	}
+namespace FYP.Score{
+	
+	[System.Serializable]
+	public class PlayerScore {
+		public string name;
+		public int score;
+	};
 
-	public static int GetLocalPlayerScore(){
-		return PhotonNetwork.player.GetScore ();
+	public sealed class Scoreboard : MonoBehaviour {
+		
+		public static void  AddLocalPlayerScore(int score){
+			PhotonNetwork.player.AddScore (score);
+			Debug.Log ("all player score: " + GetAllPlayerScore ());
+		}
+
+		public static int GetLocalPlayerScore(){
+			return PhotonNetwork.player.GetScore ();
+		}
+
+		public static List<PlayerScore> GetAllPlayerScore(){
+			List<PlayerScore> scoreList = new List<PlayerScore> ();
+			foreach (PhotonPlayer player in PhotonNetwork.playerList) {
+				PlayerScore temp = new PlayerScore ();
+				temp.name = player.name;
+				temp.score = player.GetScore ();
+				scoreList.Add (temp);
+			}
+			return scoreList;
+		}
 	}
 
 }
