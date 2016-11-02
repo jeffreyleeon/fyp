4254c137a3303b369e92741e549c1503dd24651a
Lee Chun On <jeffreyleeon@gmail.com>
2016-11-02 23:17:19 +0800

    Rotate the knife

---
+++ Assets/Scripts/Weapons/Knife.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knife : Bullet {
+
+	[Tooltip("Revolutions the knife will rotate per second")]
+	public int rotationsPerSec;
+
+	void Start () {
+		transform.Rotate(-90.0f, 0.0f, 0.0f);
+	}
+
+	void Update () {
+		transform.Rotate(rotationsPerSec * 6.0f * Time.deltaTime, 0.0f, 0.0f);
+	}
+
+}
