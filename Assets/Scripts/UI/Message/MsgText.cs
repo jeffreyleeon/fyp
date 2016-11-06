28a703fc472656342844a137a5e3a6c7cb9392ab
johnny <cywongaw@stu.ust.hk>
2016-11-06 23:23:59 +0800

    create message system with observer pattern

---
+++ Assets/Scripts/UI/Message/MsgText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+
+public abstract class MsgObserver : MonoBehaviour {
+
+	public abstract void ShowMessage (string msg);
+}
+
+public class MsgText : MsgObserver {
+
+	public Text txt;
+
+	void Start(){
+		MsgSystem.AddObserver (this);
+	}
+
+	void OnDestroy () {
+		MsgSystem.RemoveObserver (this);
+	}
+
+	public override void ShowMessage (string msg){
+		txt.text = msg;
+	}
+
+}
