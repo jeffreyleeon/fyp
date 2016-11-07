12d7aa74a746a8ee4e9e663d4b9efa43266175c9
Jeffrey Lee <jeffreyleeon@gmail.com>
2016-11-07 20:36:42 +0800

    Implement raycast and able to detect a button

---
+++ Assets/Scripts/PointingSystem/RayCast.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, forward, out hit)) {
+            print("=================hitting " + hit.collider.gameObject.name);
+            print("LINE BREAK\n" + Time.time);
+        }
+	}
+}
