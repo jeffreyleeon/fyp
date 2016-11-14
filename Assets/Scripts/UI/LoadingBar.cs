5a384348c7bf241200ca449ebec9b3c87d840f05
Alice Sin <sinwinglamalice@gmail.com>
2016-11-14 21:59:35 +0800

    Add script - LoadingBar to update the radial

---
+++ Assets/Scripts/UI/LoadingBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadingBar : MonoBehaviour {
+
+	public Transform loadingBar;
+	public static float currentAmount = 0;
+
+	// Update is called once per frame
+	void Update () {
+		loadingBar.GetComponent<Image> ().fillAmount = currentAmount / 60;
+	}
+}
