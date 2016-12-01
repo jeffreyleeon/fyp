34e5940e23de2b1bd1f8ba807e899f72518fa2d9
Jeffrey Lee <jeffreyleeon@gmail.com>
2016-12-01 15:02:06 +0800

    Refractor code of pointing system

--- Assets/Scripts/PointingSystem/RayCast.cs
+++ Assets/Scripts/PointingSystem/RayCast.cs
@@ -10,16 +10,17 @@ public class RayCast : MonoBehaviour {
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
         Debug.DrawRay(transform.position, forward, Color.green);
 
-        if (Physics.Raycast(transform.position, forward, out hit)) {
-            print("=================hitting " + hit.collider.gameObject.name);
-            print("LINE BREAK\n" + Time.time);
+		if (Physics.Raycast (transform.position, forward, out hit)) {
+			print ("=================hitting " + hit.collider.gameObject.name);
+			print ("LINE BREAK\n" + Time.time);
 
-			if (hit.collider.gameObject.name == "StartButton") {
-				StartSceneManager.EnableLoading();
-				StartSceneManager.AddCount();
+			if (hit.collider.gameObject.name == ObjectStore.GetStartButtonName ()) {
+				StartSceneManager.EnableLoading ();
+				StartSceneManager.SetTargetScene (StartSceneManager.BRIGHT_SCENE);
+				StartSceneManager.AddCount ();
 			} else {
-				StartSceneManager.DisableLoading();
+				StartSceneManager.DisableLoading ();
 			}
-        }
+		}
 	}
 }
