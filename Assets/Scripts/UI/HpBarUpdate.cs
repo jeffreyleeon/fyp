dfff4910587476edb2b212ac124d23827f6a56ba
johnny <cywongaw@stu.ust.hk>
2016-12-02 09:42:58 +0800

    set hpBar move with current health

--- Assets/Scripts/UI/HpBarUpdate.cs
+++ Assets/Scripts/UI/HpBarUpdate.cs
@@ -5,14 +5,19 @@ using System.Collections;
 public class HpBarUpdate : MonoBehaviour {
 
 	public RectTransform hpBar;
-	private int Hp = 100;
+	public int currHP;
+	public int maxHP = 100;
+	public float maxRectWidth;
 
 	// Use this for initialization
 	void Start () {
+		currHP = maxHP;
+		maxRectWidth = hpBar.rect.width;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		hpBar.sizeDelta = new Vector2(Mathf.Clamp(hpBar.rect.width - 0.2f, 0, 100), hpBar.rect.height);
+		float hpBarRatio = ((float)currHP / (float)maxHP) * maxRectWidth;
+		hpBar.sizeDelta = new Vector2(Mathf.Clamp(hpBarRatio, 0, maxRectWidth), hpBar.rect.height);
 	}
 }
