053302ca73c00af2efb006f2fcc5128b4803f7d2
Lee Chun On <jeffreyleeon@gmail.com>
2016-12-02 16:54:56 +0800

    Fix knight orientation and add walking animation

--- Assets/Scripts/SpawnEnemy/KnightEnemy.cs
+++ Assets/Scripts/SpawnEnemy/KnightEnemy.cs
@@ -5,19 +5,23 @@ public class KnightEnemy : WalkingEnemy {
 
 	Animator anim;
 	int jumpHash = Animator.StringToHash("Jump");
+	int walkHash = Animator.StringToHash("Walk");
 	int count = 0;
 
 	// Use this for initialization
 	virtual public void Start () {
 		base.Start ();
 		anim = GetComponent<Animator>();
+		transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
 	}
 	
 	// Update is called once per frame
 	virtual public void Update () {
 		base.Update ();
-		if (count % 360 == 0) {
+		if (count % 360 < 100) {
 			anim.SetTrigger (jumpHash);
+		} else {
+			anim.SetTrigger (walkHash);
 		}
 		count++;
 	}
