dece59384cf4b6b38383e675e728ac8cf69e82c3
johnny <cywongaw@stu.ust.hk>
2016-12-27 22:28:06 +0800

    add EndScore script for score display in score scene

---
+++ Assets/Scripts/UI/EndScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using FYP.Score;
+
+public class EndScore : MonoBehaviour {
+
+	public Text txt;
+	public List<PlayerScore> playersScore;
+
+	// Use this for initialization
+	void Start () {
+		
+		txt.text = "";
+		playersScore = Scoreboard.GetAllPlayerScore ();
+		foreach (PlayerScore player in playersScore) {
+			txt.text += player.name + "\t\t\t\t\t\t" + player.score + "\n";
+
+		}
+	}
+	
+
+}
