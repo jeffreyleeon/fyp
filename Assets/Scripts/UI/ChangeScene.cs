dbe61a7a4689b00a4beb969b6aa0ec903f9ebcc3
johnny <cywongaw@stu.ust.hk>
2017-01-08 13:03:15 +0800

    all broadcastchangescene function

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour {
+public class ChangeScene : Photon.MonoBehaviour {
 
 	public static int BRIGHT_SCENE = 1;
 	public static int TUTORIAL_SCENE = 2;
@@ -11,4 +11,10 @@ public class ChangeScene : MonoBehaviour {
 	public static void ChangeToScene (int sceneIndex) {
 		SceneManager.LoadScene (sceneIndex);
 	}
+
+	[PunRPC]
+	public static void BroadcastChangeToScene(int sceneIndex) {
+		ChangeToScene (sceneIndex);
+	}
 }
+
