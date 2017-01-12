8ccf243465fd1c41ec0416b7ad6264e30c2c69cd
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-01-12 21:02:59 +0800

    Import Magical library

---
+++ Assets/CustomLibrary/Magical/Sources/Scripts/FX_AddForceForward.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MagicalFX
+{
+	[RequireComponent (typeof(Rigidbody))]
+
+public class FX_AddForceForward : MonoBehaviour
+	{
+	
+		public float Force = 300;
+
+		void Start ()
+		{
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			if (rb) {
+				rb.AddForce (this.transform.forward * Force);
+			}
+		}
+
+		void Update ()
+		{
+		
+		}
+	}
+}
---
+++ Assets/CustomLibrary/Magical/Sources/Scripts/FX_HitSpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MagicalFX
+{
+	public class FX_HitSpawner : MonoBehaviour
+	{
+
+
+		public GameObject FXSpawn;
+		public bool DestoyOnHit = false;
+		public bool FixRotation = false;
+		public float LifeTimeAfterHit = 1;
+		public float LifeTime = 0;
+	
+		void Start ()
+		{
+		
+		}
+	
+		void Spawn ()
+		{
+			if (FXSpawn != null) {
+				Quaternion rotate = this.transform.rotation;
+				if (!FixRotation)
+					rotate = FXSpawn.transform.rotation;
+				GameObject fx = (GameObject)GameObject.Instantiate (FXSpawn, this.transform.position, rotate);
+				if (LifeTime > 0)
+					GameObject.Destroy (fx.gameObject, LifeTime);
+			}
+			if (DestoyOnHit) {
+			
+				GameObject.Destroy (this.gameObject, LifeTimeAfterHit);
+				Collider collider = this.gameObject.GetComponent<Collider> ();
+				if (collider)
+					collider.enabled = false;
+
+			}
+		}
+	
+		void OnTriggerEnter (Collider other)
+		{
+			Spawn ();
+		}
+	
+		void OnCollisionEnter (Collision collision)
+		{
+			Spawn ();
+		}
+	}
+}
