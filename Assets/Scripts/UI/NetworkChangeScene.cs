3ddc2b660ca83203a9b9496cbbc2a9693373dc04
johnny <cywongaw@stu.ust.hk>
2017-01-13 00:14:21 +0800

    move broadcast to Scenemanager, add scenemanager to brightscene

---
+++ Assets/Scripts/UI/NetworkChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkChangeScene : Photon.MonoBehaviour {
+
+	[PunRPC]
+	public void BroadcastChangeToScene(int sceneIndex) {
+		ChangeScene.ChangeToScene (sceneIndex);
+	}
+}
