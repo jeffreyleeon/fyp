62caee84994e85c5ee43f9cf4dde2490a1f4a367
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-01-17 13:09:07 +0800

    Added a plain for video playing

---
+++ Assets/Movie/Video.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Video : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+		Renderer renderer = GetComponent<Renderer> ();
+		renderer.material.mainTexture = movie as MovieTexture;
+		movie.Play ();
+	}
+}
