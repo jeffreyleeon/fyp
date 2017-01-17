ae6e65cd39ecd19c07e71331b331698c1dbdb1bf
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-01-17 12:50:51 +0800

    Scripts for panning the camera

---
+++ Assets/Scripts/VideoCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoCamera : MonoBehaviour {
+
+	public Transform from;
+	public Transform to;
+	public float speed = 0.001F;
+	private float startedTime;
+
+	private bool started = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space) && !started) {
+			started = true;
+			startedTime = Time.time;
+		}
+		if (started) {
+			Action ();
+		}
+	}
+
+	void Action () {
+		// transform.position = Quaternion.Lerp (from.position, to.position, (Time.time - startedTime) * speed);
+		transform.rotation = Quaternion.Lerp (from.rotation, to.rotation, (Time.time - startedTime) * speed);
+	}
+}
