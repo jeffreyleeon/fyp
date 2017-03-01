13b15a96c0013f57696a82e3e35b9de9eb56fcca
johnny <cywongaw@stu.ust.hk>
2017-03-01 20:19:23 +0800

    move disableUI part to a seperate function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -82,17 +82,19 @@ public class GameManager : Photon.PunBehaviour {
 			deathPanel.GetComponent<Image> ().CrossFadeAlpha (0f, 1.0f, false);
 			trinus.transform.position = new Vector3 (0, 10, -10);
 			ObjectStore.FindMyPlayer().Kill();
-			GameObject[] UI_HP = ObjectStore.FindUIHP ();
-			foreach (GameObject i in UI_HP) {
-				i.gameObject.SetActive (false);
-			}
-			GameObject[] UI_Weapon = ObjectStore.FindUIWeapon ();
-			foreach (GameObject i in UI_Weapon) {
-				i.gameObject.SetActive (false);
-			}
+			DisableUI ();
 		}
+	}
 
-
+	private void DisableUI(){
+		GameObject[] UI_HP = ObjectStore.FindUIHP ();
+		foreach (GameObject i in UI_HP) {
+			i.gameObject.SetActive (false);
+		}
+		GameObject[] UI_Weapon = ObjectStore.FindUIWeapon ();
+		foreach (GameObject i in UI_Weapon) {
+			i.gameObject.SetActive (false);
+		}
 	}
 
 	#endregion
