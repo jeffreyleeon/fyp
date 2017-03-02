da9069523a9da002fdfee3c86029106ee19aafd7
Alice Sin <sinwinglamalice@gmail.com>
2017-03-02 14:17:00 +0800

    Import boss of horror scene

---
+++ Assets/Scripts/SpawnEnemy/UndeadEnemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UndeadEnemy : WalkingEnemy {
+
+	Animator anim;
+	string walkHash = "Animal 01 Gorilla";
+	string idleHash = "Idle 02";
+
+	// Use this for initialization
+	virtual public void Start () {
+		base.Start ();
+		// initialising reference variables
+		anim = gameObject.GetComponent<Animator>();
+
+		transform.rotation = Quaternion.AngleAxis (180, Vector3.up);
+		string action = GetInitialAction ();
+		anim.Play (action);
+	}
+
+	string GetInitialAction () {
+		string ret = idleHash;
+
+		if (!isIdle) {
+			ret = walkHash;
+		}
+
+		return ret;
+	}
+
+	// Update is called once per frame
+	virtual public void Update () {
+		base.Update ();
+	}
+}
