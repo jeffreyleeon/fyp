fdc0baf56bfc02edb942125493f22336ca68472a
johnny <cywongaw@stu.ust.hk>
2017-03-05 16:05:46 +0800

    add scenesetting script, script is attached to SceneManager object

---
+++ Assets/Scripts/Utils/SceneSetting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSetting : MonoBehaviour {
+	GameObject trinus;
+	GameObject leap;
+
+
+	// Use this for initialization
+	void Start () {
+		trinus = ObjectStore.FindTrinus ();
+		leap = ObjectStore.FindLeapMotionController ();
+
+		switch (CurrentSceneIndex()){
+		case ChangeScene.MENU_SCENE:
+		case ChangeScene.SCORE_SCENE:
+			trinus.transform.position = new Vector3 (0, 0, 0);
+			leap.SetActive (false);
+			break;	
+		case ChangeScene.BRIGHT_SCENE:
+			leap.SetActive (true);
+			break;
+			
+		}
+	}
+	
+	void OnDestroy(){
+		//reenable leap, otherwise objectstore cannot find
+		leap.SetActive (true);
+	}
+
+	private int CurrentSceneIndex(){
+		return SceneManagerHelper.ActiveSceneBuildIndex;
+	}
+}
