e3ed36314a2e347d5c324d926a95bec88f6048bd
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-03-15 20:51:42 +0800

    Able to change simple weapon in tutorial page

---
+++ Assets/Scripts/TutorialShootingController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialShootingController : ShootingController {
+	
+	[Tooltip("Prefab of secondary bullet type for teaching player to change weapon")]
+	public GameObject secondaryBulletPrefab;
+
+	override protected void ChangeWeapon () {
+		GameObject temp = bulletPrefab;
+		bulletPrefab = secondaryBulletPrefab;
+		secondaryBulletPrefab = temp;
+	}
+
+}
