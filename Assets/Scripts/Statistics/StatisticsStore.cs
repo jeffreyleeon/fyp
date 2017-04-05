568495da771c4eb57d25aa2349ead354a4c7b12a
Lee Chun On <jeffreyleeon@gmail.com>
2017-04-05 17:20:42 +0800

    Add StatisticsStore singleton

---
+++ Assets/Scripts/Statistics/StatisticsStore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class StatisticsStore : MonoBehaviour {
+
+	private static StatisticsStore instance = new StatisticsStore ();
+
+	// Variables related to statistics
+
+	private StatisticsStore () {
+		ResetStat ();
+	}
+
+	public static StatisticsStore GetInstance () {
+		return instance;
+	}
+
+	public void ResetStat () {
+	
+	}
+
+}
