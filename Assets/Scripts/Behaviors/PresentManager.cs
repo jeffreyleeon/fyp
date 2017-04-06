8fc7014e037a4151a708cb13fe87eb186f28fcaf
Lee Chun On <jeffreyleeon@gmail.com>
2017-04-07 00:05:05 +0800

    Create PresentBehavior scripts

---
+++ Assets/Scripts/Behaviors/PresentManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresentManager : MonoBehaviour {
+
+	static public string presentsIconBasePath = "PresentsIcon/";
+	public enum PresentType {
+		Shield,
+		HalfDamage,
+		AddHP,
+	}
+}
+
+public interface IPresent {
+	PresentManager.PresentType PresentType ();
+	string GetPresentIcon ();
+	void RedeemEffect ();
+}
+
+public class ShieldBehv : MonoBehaviour, IPresent {
+	public PresentManager.PresentType PresentType () {
+		return PresentManager.PresentType.Shield;
+	}
+
+	public string GetPresentIcon () {
+		return "";
+	}
+
+	public void RedeemEffect (){
+		Debug.Log ("======Redeem effect: Shield");
+	}
+}
+
+public class HalfDamageBehv : MonoBehaviour, IPresent {
+	public PresentManager.PresentType PresentType () {
+		return PresentManager.PresentType.HalfDamage;
+	}
+
+	public string GetPresentIcon () {
+		return "";
+	}
+
+	public void RedeemEffect (){
+		Debug.Log ("======Redeem effect: Half damage");
+	}
+}
+
+public class AddHPBehv : MonoBehaviour, IPresent {
+	public PresentManager.PresentType PresentType () {
+		return PresentManager.PresentType.AddHP;
+	}
+
+	public string GetPresentIcon () {
+		return "";
+	}
+
+	public void RedeemEffect (){
+		Debug.Log ("======Redeem effect: Add HP");
+	}
+}
