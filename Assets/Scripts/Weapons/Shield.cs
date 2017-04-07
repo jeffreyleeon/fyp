d0615fc52017b52ac61af8ad1b3e1525929d611c
Lee Chun On <jeffreyleeon@gmail.com>
2017-04-08 01:28:56 +0800

    Add a shield prefab, script and shader

---
+++ Assets/Scripts/Weapons/Shield.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Renderer r = gameObject.AddComponent<MeshRenderer>() as Renderer;
+		Shader s = Shader.Find ("Unlit/Transparent");
+//		Shader s = Shader.Find ("Transparent/Diffuse");
+		r.material = new Material(s);
+		r.material.color = new Color (1, 1, 1, 0.5f);
+	}
+
+}
