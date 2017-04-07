6ed2739d609965f700450b3fdc3847f4ef793477
Lee Chun On <jeffreyleeon@gmail.com>
2017-04-08 01:39:58 +0800

    Remove unused code

--- Assets/Scripts/Weapons/Shield.cs
+++ Assets/Scripts/Weapons/Shield.cs
@@ -7,9 +7,7 @@ public class Shield : MonoBehaviour {
 	void Start () {
 		Renderer r = gameObject.AddComponent<MeshRenderer>() as Renderer;
 		Shader s = Shader.Find ("Unlit/Transparent");
-//		Shader s = Shader.Find ("Transparent/Diffuse");
 		r.material = new Material(s);
-		r.material.color = new Color (1, 1, 1, 0.5f);
 	}
 
 }
