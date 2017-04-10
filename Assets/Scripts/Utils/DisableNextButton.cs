70894fca03e88d71fc761aba8c40af7c9209f87c
Wong Chun Yin <johnny@Wongs-MacBook-Pro.local>
2017-04-10 16:21:36 +0800

    disable next button in horror scene

---
+++ Assets/Scripts/Utils/DisableNextButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableNextButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (CurrentLevel.currentLevel == ChangeScene.HORROR_SCENE) {
+			this.gameObject.SetActive(false);
+		}
+	}
+
+}
