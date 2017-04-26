600a0b4a7964e56c33c2c45f0bcdb37de5bba872
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-04-26 18:03:13 +0800

    Update Trailor and Menu scene index to make game start at trailor

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	public const int MENU_SCENE = 0;
+	public const int MENU_SCENE = 8;
 	public const int BRIGHT_SCENE = 1;
 	public const int TUTORIAL_SCENE = 2;
 	public const int SCORE_SCENE = 3;
@@ -12,7 +12,7 @@ public class ChangeScene : MonoBehaviour {
 	public const int SPACE_SCENE = 5;
 	public const int CHRISTMAS_SCENE = 6;
 	public const int HORROR_SCENE = 7;
-	public const int TRAILOR_SCENE = 8;
+	public const int TRAILOR_SCENE = 0;
 
 	public static void ChangeToScene (int sceneIndex) {
 		if (sceneIndex == SCORE_SCENE) {
