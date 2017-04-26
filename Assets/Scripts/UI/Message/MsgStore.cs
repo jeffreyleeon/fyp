7bc019dbaf205a9239fef5e51790236072cd60b8
Alice Sin <sinwinglamalice@gmail.com>
2017-04-26 23:38:44 +0800

    Correct word - "plam" to "palm" in tutorial scene

--- Assets/Scripts/UI/Message/MsgStore.cs
+++ Assets/Scripts/UI/Message/MsgStore.cs
@@ -8,7 +8,7 @@ public class MsgStore {
 	private const string showHandTutorMsg = "Place both of your hands in front of the camera";
 	private const string dismissHandTutorMsg = "Put down your hands and you will not see them in the screen";
 	private const string shootingTutorMsg = "Open your palm to shoot bullets";
-	private const string changeWeaponTutorMsg = "Close your plam and shake your hand to change bullet type";
+	private const string changeWeaponTutorMsg = "Close your palm and shake your hand to change bullet type";
 	private const string shootingEnemiesTutorMsg = "Aim and kill all the enemies!";
 	private const string tutorialEndMsgPartOne = "End of the tutorial, ready to start the first stage in ";
 	private const string tutorialEndMsgPartTwo = " seconds!";
