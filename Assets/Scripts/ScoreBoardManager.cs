f01a17f746eee0fd3ef03bcde0fd87df6b513848
Jeffrey Lee <jeffreyleeon@gmail.com>
2017-04-28 12:53:43 +0800

    Press space to back to menu page from score page

--- Assets/Scripts/ScoreBoardManager.cs
+++ Assets/Scripts/ScoreBoardManager.cs
@@ -7,6 +7,19 @@ public class ScoreBoardManager : MonoBehaviour {
 		RenderSettings.skybox = ObjectStore.ActiveSkybox;
 	}
 
+	// Update is called once per frame
+	void Update () {
+		// Debug usage
+		ListenKeyboard ();
+	}
+
+	void ListenKeyboard () {
+		if (Input.GetKey (KeyCode.Space)) {
+			// Enter bright scene directly
+			ChangeScene.ChangeToScene (ChangeScene.MENU_SCENE);
+		}
+	}
+
 	void OnDestroy () {
 		GameObject decoration = ObjectStore.FindDecoration ();
 		Destroy (decoration);
